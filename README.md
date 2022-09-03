24c4ae78dc647df7a7ee8c6c663d040d5fc83f04
hansbrattberg <hans.brattberg@crisp.se>
2022-09-03 15:03:25 +0200

    remove hard coded securityToken

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # entsoe_dayahead
+
+node start.mjs <securityToken>
+
+To get a security token you need to request one by
+sending an email to transparency@entsoe.eu with headline "Restful API access"
+
--- start.mjs
+++ start.mjs
@@ -3,44 +3,49 @@ import convert from 'xml-js'
 import moment from 'moment'
 
 // https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html
-const priceAreaCode = {}
-priceAreaCode['SE1'] = '10Y1001A1001A44P'
-priceAreaCode['SE2'] = '10Y1001A1001A45N'
-priceAreaCode['SE3'] = '10Y1001A1001A46L'
-priceAreaCode['SE4'] = '10Y1001A1001A47J'
 
-const documentType = 'A44'   // Price Document
+if(process.argv[2] == undefined) {
+    console.log("Please provide a securityToken as the first parameter, that you get from entsoe!")
+} else {
 
-const securityToken = '2db036cb-892a-41da-b854-30fe5bf08553'   // send an email to transparency@entsoe.eu with headline "Restful API access" to get a token
-const in_Domain = priceAreaCode['SE3']
-const out_Domain = in_Domain
+    const securityToken = process.argv[2]
+    const priceAreaCode = {}
+    priceAreaCode['SE1'] = '10Y1001A1001A44P'
+    priceAreaCode['SE2'] = '10Y1001A1001A45N'
+    priceAreaCode['SE3'] = '10Y1001A1001A46L'
+    priceAreaCode['SE4'] = '10Y1001A1001A47J'
+    const documentType = 'A44'   // Price Document
 
-const x = moment().startOf('day')
-const periodStart = now.utc().format('YYYYMMDD2300')  // 23 winter time, works also for summer when the result will be from 22
-const periodEnd = now.utc().add(1, 'day').format('YYYYMMDD2300')
+    const in_Domain = priceAreaCode['SE3']
+    const out_Domain = in_Domain
 
-const params =
-    'securityToken=' + securityToken +
-    '&' + 'documentType=' + documentType +
-    '&' + 'in_Domain=' + in_Domain +
-    '&' + 'out_Domain=' + out_Domain +
-    '&' + 'periodStart=' + periodStart +
-    '&' + 'periodEnd=' + periodEnd
+    const now = moment().startOf('day')
+    const periodStart = now.utc().format('YYYYMMDD2300')  // 23 winter time, works also for summer when the result will be from 22
+    const periodEnd = now.utc().add(1, 'day').format('YYYYMMDD2300')
 
-const url = 'https://transparency.entsoe.eu/api?' + params
-console.log(url)
+    const params =
+        'securityToken=' + securityToken +
+        '&' + 'documentType=' + documentType +
+        '&' + 'in_Domain=' + in_Domain +
+        '&' + 'out_Domain=' + out_Domain +
+        '&' + 'periodStart=' + periodStart +
+        '&' + 'periodEnd=' + periodEnd
 
-axios.get(url)
-    .then(res => {
-        console.log('Status Code:', res.status)
+    const url = 'https://transparency.entsoe.eu/api?' + params
+    console.log(url)
 
-        const json = convert.xml2json(res.data, {compact: true, spaces: 4})
-        console.log(JSON.parse(json).Publication_MarketDocument.TimeSeries.Period.timeInterval)
+    axios.get(url)
+        .then(res => {
+            console.log('Status Code:', res.status)
 
-        for (let i = 0; i < 24; i++) {
-            console.log(JSON.parse(json).Publication_MarketDocument.TimeSeries.Period.Point[i])
-        }
-    })
-    .catch(err => {
-        console.log('Message: ', err.message);
-    });
+            const json = convert.xml2json(res.data, {compact: true, spaces: 4})
+            console.log(JSON.parse(json).Publication_MarketDocument.TimeSeries.Period.timeInterval)
+
+            for (let i = 0; i < 24; i++) {
+                console.log(JSON.parse(json).Publication_MarketDocument.TimeSeries.Period.Point[i])
+            }
+        })
+        .catch(err => {
+            console.log('Message: ', err.message);
+        });
+}
